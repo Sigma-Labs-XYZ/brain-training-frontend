647e7a34f7d88f69d32ce61279f5515596f81115
Kamilah <kamilahmohchin@gmail.com>
2022-06-06 16:07:37 +0100

    created PlayerDisplayCard component

--- brain-training/src/Components/HomePage.js
+++ brain-training/src/Components/HomePage.js
@@ -5,12 +5,14 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import PlayerDisplayCard from "./PlayerDisplayCard";
 
 const theme = createTheme();
 
 export default function HomePage() {
   return (
     <ThemeProvider theme={theme}>
+      <PlayerDisplayCard />
       <main>
         <Box
           sx={{
---
+++ brain-training/src/Components/PlayerDisplayCard.js
@@ -0,0 +1,7 @@
+export default function PlayerDisplayCard() {
+  return (
+    <div>
+      <h1>Player display card </h1>
+    </div>
+  );
+}
