ec5e2a3496a74a69ccb4dbe18bade43fb6827079
Kamilah <kamilahmohchin@gmail.com>
2022-06-07 16:08:46 +0100

    PlayerDisplayCard finished.

--- brain-training/src/Components/PlayerDisplayCard.js
+++ brain-training/src/Components/PlayerDisplayCard.js
@@ -2,15 +2,25 @@ import { getProfile } from "./Networking";
 import { Box, Container } from "@mui/system";
 
 export default function PlayerDisplayCard() {
-  async function fetchProfile() {
-    const playerAvatar = await getProfile(2);
-    return playerAvatar;
+  async function fetchProfilePicture() {
+    const profile = await getProfile(2);
+    return profile.user.profile_picture_id;
   }
+  async function fetchProfileUsername() {
+    const profile = await getProfile(2);
+    return profile.user.username;
+  }
+
+  async function fetchWinMessage() {
+    const profile = await getProfile(2);
+    return profile.user.win_message;
+  }
+
   return (
     <div>
       <Container>
         <Box sx={{ justifyContent: "space-between", width: 300, height: 300 }}>
-          {fetchProfile()}
+          {`${fetchProfilePicture()} ${fetchWinMessage()} ${fetchProfileUsername()} `}
         </Box>
       </Container>
     </div>
